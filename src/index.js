8befc91c3f5625e3d433cc4c7dc004ca4ab9c2ea
Vinayak Raju <vinayak.raju@salesforce.com>
2020-08-01 19:47:04 +0530

    module 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-export function fetchMovies(){
+const movies = require("./data/movies.json")
+import {fetchWithTimeout} from './services.js'
 
+export function fetchMovies(){
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
+
+var moviePromise = fetchMovies();
+
+moviePromise.then(result => console.log(result));

adc9fe855d448d87ea14c264b15f353a4649e51a
Vinayak Raju <vinayak.raju@salesforce.com>
2020-08-02 12:02:02 +0530

    Module 7 Changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
+import { fetchWithTimeout, fetchMovies, fetchBooks, asyncFetchMovies, asyncFetchBooks } from "./services";
 const movies = require("./data/movies.json");
 
 const getBooksAndMovies = () => {
@@ -26,4 +26,36 @@ function getBooksOrMovies() {
 const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {
   console.log("getBooksOrMoviesPromise", results);
+});
+
+async function getBooksAndMoviesAsync(){
+    try{
+        const [books,movies] = await Promise.all([asyncFetchBooks(),asyncFetchMovies()]);
+        return {books,movies};
+    } catch(error){
+        console.log("Error fetching books and movies", error);
+    }
+}
+
+async function getBooksOrMoviesAsync(){
+    try {
+        const values = await Promise.race([asyncFetchBooks(),asyncFetchMovies()]);
+        return values;
+    } 
+    catch(error){
+        console.log("Error fetching books and movies", error);
+    }
+}
+
+getBooksAndMoviesAsync().then(results => {
+    console.log("movies and books", {
+        movies: results.movies,
+        books: results.books
+      });
+});
+
+getBooksOrMoviesAsync().then(results =>{
+    console.log("movies OR books", {
+        results
+    });
 });

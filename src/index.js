ea83cdea9362a0a1b651179166ee583cba5ed3a9
Vinayak Raju <vinayak.raju@salesforce.com>
2020-08-02 11:25:53 +0530

    Module 4 Changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import {fetchWithTimeout} from './services'
-const movies = require("./data/movies.json");
+import {fetchMovies, fetchBooks} from './services'
 
-
-export function fetchMovies(){
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies(){
+    return Promise.all([fetchBooks(),fetchMovies()])
+                .then(([books, movies ]) => ({
+                    books,
+                    movies
+                }))
+                .catch(error => console.log("Error fetching books and movies", error));
 }
 
-var moviePromise = fetchMovies();
+const getBooksAndMoviesPromise = getBooksAndMovies();
 
-moviePromise.then(results => {console.log(results);});
+getBooksAndMoviesPromise.then(results=>{
+    console.log('getBooksAndMoviesPromise', results);
+})
+
+function getBooksOrMovies(){
+    return Promise.race([fetchBooks(), fetchMovies()])
+                .then(results => results)
+                .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+
+getBooksOrMoviesPromise.then((results)=>{
+    console.log('getBooksOrMoviesPromise', results);
+})

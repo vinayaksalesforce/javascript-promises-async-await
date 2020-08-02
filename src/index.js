34003003163e20bd29bda6cc8df94e9c962e818b
Vinayak Raju <vinayak.raju@salesforce.com>
2020-08-02 12:35:13 +0530

    Module 8 Changes

--- src/index.js
+++ src/index.js
@@ -45,17 +45,13 @@ import {
       };
     } catch (error) {
       console.log("Error fetching books and movies", error);
+      return error;
     }
   }
   
   async function getBooksOrMoviesAsync() {
-    try {
-      const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+      const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
       return values;
-    } catch (error) {
-      console.error("Error waiting for the promise race", error);
-      throw error;
-    }
   }
   
   getBooksAndMoviesAsync().then(results => {
